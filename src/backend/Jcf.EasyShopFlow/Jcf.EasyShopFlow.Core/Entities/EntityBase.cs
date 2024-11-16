c9c9aa022862b58a3c7201bfebadaaf4eab4b50a
Joao Carias de Franca <joaocariasdefranca@gmail.com>
2024-11-16 17:51:22 -0300

    [Backend] Criando entidade base e entidade de usuário

---
+++ src/backend/Jcf.EasyShopFlow/Jcf.EasyShopFlow.Core/Entities/EntityBase.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jcf.EasyShopFlow.Core.Entities
+{
+    public abstract class EntityBase
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; private set; }
+
+        [Required]
+        public bool IsActive { get; private set; } = true;
+
+        public DateTime CreateAt { get; private set; } = DateTime.Now;
+
+        public User? UserCreate { get; private set; } 
+
+        public DateTime? UpdateAt { get; private set; }
+
+        public User? UserUpdate { get; private set; }
+        
+        public EntityBase() { }
+    }
+}
---
+++ src/backend/Jcf.EasyShopFlow/Jcf.EasyShopFlow.Core/Entities/User.cs
@@ -0,0 +1,40 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Jcf.EasyShopFlow.Core.Entities
+{
+    public class User : EntityBase
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; private set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; private set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; private set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Login {  get; private set; }
+
+        public User(string name, string email, string password, string login)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            Login = login;
+        }
+
+        private User()
+        {
+            Name = string.Empty;
+            Email = string.Empty;
+            Password = string.Empty;
+            Login = string.Empty;
+        }
+    }
+}
